6daae022a08975567ac0bcd2d847efdfb960c391
Thomas Capelle <thomascapelle@gmail.com>
2019-09-26 17:25:24 +0200

    up and running full UCR

--- ucr.py
+++ ucr.py
@@ -1,10 +1,62 @@
+import fastai
+import torch
+import torch.nn as nn
+from resnet import create_resnet
+from utils import *
+from download import unzip_data
+from fastai.script import *
+from fastai.vision import *
+
 "runs resnet over UCR dataset"
 
-flist = ['Adiac', 'Beef', 'CBF', 'ChlorineConcentration', 'CinC_ECG_torso', 'Coffee', 'Cricket_X', 'Cricket_Y', 'Cricket_Z', 
-'DiatomSizeReduction', 'ECGFiveDays', 'FaceAll', 'FaceFour', 'FacesUCR', '50words', 'FISH', 'Gun_Point', 'Haptics', 
-'InlineSkate', 'ItalyPowerDemand', 'Lighting2', 'Lighting7', 'MALLAT', 'MedicalImages', 'MoteStrain', 'NonInvasiveFatalECG_Thorax1', 
-'NonInvasiveFatalECG_Thorax2', 'OliveOil', 'OSULeaf', 'SonyAIBORobotSurface', 'SonyAIBORobotSurfaceII', 'StarLightCurves', 'SwedishLeaf', 'Symbols', 
-'synthetic_control', 'Trace', 'TwoLeadECG', 'Two_Patterns', 'uWaveGestureLibrary_X', 'uWaveGestureLibrary_Y', 'uWaveGestureLibrary_Z', 'wafer', 'WordsSynonyms', 'yoga']
 
-if __name__=='__main__':
-    DATAPATH = unzip_data()
+def get_ds(df):
+    return TensorDataset(torch.Tensor(df.values[:,:-1].astype('float')).unsqueeze(dim=1), 
+                         torch.Tensor(df.values[:,-1].astype('int')).long())
+
+def max_bs(N):
+    N = N//6
+    k=1
+    while (N//2**k)>1: k+=1
+    return 2**k
+
+def create_databunch(tr_ds, val_ds, bs=64):
+    train_dl = DataLoader(tr_ds, batch_size=bs, shuffle=True)
+    valid_dl = DataLoader(val_ds, batch_size=bs, shuffle=True)
+    return DataBunch(train_dl, valid_dl)
+
+def train_task(path, task='Adiac', epochs=40, lr=5e-4):
+    df_train, df_test = load_df(path, task)
+
+    df_train, ts_train = cleanup(df_train)
+    df_test, ts_test = cleanup(df_test)
+    num_classes = df_train.target.nunique()
+    tr_ds, val_ds = get_ds(df_train), get_ds(df_test)
+    bs = max_bs(len(tr_ds))
+    db = create_databunch(tr_ds, val_ds, bs)
+    model = create_resnet(1, num_classes, ks=9, conv_sizes=[64, 128, 256, 256])
+    learn_res = fastai.basic_train.Learner(db, 
+                                       model, 
+                                       loss_func = CrossEntropyFlat(), 
+                                       metrics=[error_rate],
+                                       wd=1e-2)
+    learn_res.fit_one_cycle(epochs, lr)   
+    p, t = learn_res.get_preds() 
+    err = error_rate(p,t)                               
+    return err
+
+@call_parse
+def main(epochs:Param("Number of epochs", int)=40,
+         lr:Param("Learning rate", float)=5e-4
+         ):
+    "Training UCR for Resnet"
+    path = unzip_data()
+    summary = pd.read_csv(path/'SummaryData.csv', index_col=0)
+    flist = summary.index
+    errors = {}
+    for task in flist:
+        print(f'Training {task} ({summary.loc[task]})')
+        error = train_task(path, task, epochs, lr)
+        errors[task] = error.numpy().item()
+    print(errors)
+    pd.Series(errors).to_csv('results.csv', header=False)
