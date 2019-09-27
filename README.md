7417ac7ae309da4616b5a5c913737422d7862b7c
Thomas Capelle <thomascapelle@gmail.com>
2019-09-27 10:05:57 +0200

    updated readme

--- README.md
+++ README.md
@@ -1,9 +1,8 @@
 # TimeSeries_fastai
 
-This repository is aims to implement TimeSeries classification/regression algorithms.
+This repository aims to implement TimeSeries classification/regression algorithms. It makes extensive use of [fastai](https://github.com/fastai/fastai) training methods.
 
 ## Installation
-
 
 In short, if you have anaconda, execute:
 ```
@@ -12,8 +11,18 @@ $ conda activate timseries
 $ jupyter notebook
 ```
 
-## Notebook 01
 
-This is a basic notebook that implements the Deep Learning models proposed in [Time Series Classification from Scratch with Deep Neural Networks: A Strong Baseline](https://arxiv.org/abs/1611.06455)
+## Time Series Classification from Scratch with Deep Neural Networks: A Strong Baseline
+The original paper repo is [here](https://github.com/cauchyturing/UCR_Time_Series_Classification_Deep_Learning_Baseline) is implemented in Keras/Tf.
+- Notebook 01: This is a basic notebook that implements the Deep Learning models proposed in [Time Series Classification from Scratch with Deep Neural Networks: A Strong Baseline](https://arxiv.org/abs/1611.06455). I fine tuned the resnet architecture to get better results than the paper, and to be able to train faster.
 
-I fine tuned the resnet architecture to get better results than the paper, and to be able to train faster.
+You can also run the resnet in the full UCR data set:
+```
+$ python ucr.py
+```
+The default values are 40 epochs and `lr=5e-4`. You can modify this using the `epochs` and `lr` arguments when calling ucr.
+```
+$ python ucr.py --epochs=100 --lr=1e-3
+```
+
+The whole dataset runs in less than one our on a RTX2080ti with default settings.
--- utils.py
+++ utils.py
@@ -6,7 +6,6 @@ import seaborn as sns
 
 "this functions are based on https://github.com/mb4310/Time-Series"
 
-
 def load_df(path, task):
     dfs = []
     for file in ['TRAIN', 'TEST']:
@@ -15,21 +14,21 @@ def load_df(path, task):
         dfs.append(pd.DataFrame(data[0]))
     return dfs
     
-def cleanup(df):
-    df.columns = [k for k in range(df.shape[1]-1)]+['target']
-    for k in df.columns[:-1]:
-        df[k] = df[k].astype('float')
-    if df.target.dtype == 'object':
-        df['target'] = df['target'].apply(lambda x: x.decode('ascii')).astype('int')
-    if sorted(df.target.unique()) != list(np.arange(df.target.nunique())):
-        new_targs = pd.DataFrame({'target':df.target.unique()}).reset_index()
-        df = pd.merge(df, new_targs, left_on='target', right_on='target').drop('target',axis=1).rename(columns={'index':'target'})
-    ts = pd.melt(df.reset_index(), id_vars=['index','target'], var_name='time').rename(columns={'index':'id'})
-    ts = ts.groupby(['id','time','target']).value.mean().reset_index()
-    return df, ts
+# def cleanup(df):
+#     df.columns = [k for k in range(df.shape[1]-1)]+['target']
+#     for k in df.columns[:-1]:
+#         df[k] = df[k].astype('float')
+#     if df.target.dtype == 'object':
+#         df['target'] = df['target'].apply(lambda x: x.decode('ascii')).astype('int')
+#     if sorted(df.target.unique()) != list(np.arange(df.target.nunique())):
+#         new_targs = pd.DataFrame({'target':df.target.unique()}).reset_index()
+#         df = pd.merge(df, new_targs, left_on='target', right_on='target').drop('target',axis=1).rename(columns={'index':'target'})
+#     ts = pd.melt(df.reset_index(), id_vars=['index','target'], var_name='time').rename(columns={'index':'id'})
+#     ts = ts.groupby(['id','time','target']).value.mean().reset_index()
+#     return df, ts
 
-def graph_ts(ts):
-    "super slow"   
-    fig, axes = plt.subplots(figsize=(15,5))
-    sns.lineplot(data=ts, x='time', hue='target', y='value', units='id', estimator=None, ax=axes)
-    return None
+# def graph_ts(ts):
+#     "super slow"   
+#     fig, axes = plt.subplots(figsize=(15,5))
+#     sns.lineplot(data=ts, x='time', hue='target', y='value', units='id', estimator=None, ax=axes)
+#     return None
