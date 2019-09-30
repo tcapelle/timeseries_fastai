7a7787939e90fc6c4970b21484e37f71b679cd50
Thomas Capelle <thomascapelle@gmail.com>
2019-09-30 17:25:51 +0200

    creat_head()

--- inception.py
+++ inception.py
@@ -6,11 +6,14 @@ from fastai.vision import *
 from models import *
 
 act_fn = nn.ReLU(inplace=True)
+def create_head(ni, nout, p=0):
+    return nn.Sequential(AdaptiveConcatPool1d(),Flatten(),*bn_drop_lin(2*ni, nout, p=p))
+
 def conv(ni, nf, ks=3, stride=1, bias=False):
     return nn.Conv1d(ni, nf, kernel_size=ks, stride=stride, padding=ks//2, bias=bias)
 
 class Shortcut(Module):
-    "Merge a shortcut with the result of the module by adding them or concatenating thme if `dense=True`."
+    "Merge a shortcut with the result of the module by adding them. Adds Conv, BN and ReLU"
     def __init__(self, ni, nf, act_fn=act_fn): 
         self.act_fn=act_fn
         self.conv=conv(ni, nf, 1)
@@ -18,6 +21,7 @@ class Shortcut(Module):
     def forward(self, x): return act_fn(x + self.bn(self.conv(x.orig)))
 
 class InceptionModule(nn.Module):
+    "An inception module for TimeSeries, based on https://arxiv.org/pdf/1611.06455.pdf"
     def __init__(self, ni, nb_filters=32, kss=[41, 21, 11], use_bottleneck=True,  bottleneck_size=32,  stride=1):
         super().__init__()
         if use_bottleneck:
@@ -40,10 +44,8 @@ def create_inception(ni, nout, kss=[41, 21, 11], stride=1, depth=6, bottleneck_s
     layers = [InceptionModule(ni, kss=kss, use_bottleneck=False, stride=stride), Shortcut(1, 4*nb_filters)]
     layers += (depth-1)*[InceptionModule(4*nb_filters, kss=kss, bottleneck_size=bottleneck_size, stride=stride), 
                          Shortcut(1, 4*nb_filters)]
-    head = [AdaptiveConcatPool1d(), Flatten(), nn.Linear(8*nb_filters, nout)] if head else []
+    head = [create_head(8*nb_filters, nout)] if head else []]
     return  SequentialEx(*layers, *head)
-
-
 
 def create_inception_resnet(ni, nout, kss=[3,5,7], conv_sizes=[64, 128, 256], stride=1): 
     "A resnet with only 1 inception layer"
@@ -51,9 +53,5 @@ def create_inception_resnet(ni, nout, kss=[3,5,7], conv_sizes=[64, 128, 256], st
     sizes = zip([ni]+conv_sizes, conv_sizes)
     for n1, n2 in sizes:
         layers += [InceptionModule(n1, n2//4, kss=kss, use_bottleneck=False) if n1==1 else conv_layer(n1, n2, ks=kss[0], stride=stride), res_block_1d(n2, kss[1:3])]
-    return nn.Sequential(*layers, 
-                         AdaptiveConcatPool1d(),
-                         Flatten(),
-                        *bn_drop_lin(2*n2, nout, p=0.1)
-                        )
+    return nn.Sequential(*layers, create_head(2*n2, nout, p=0.1))
 
