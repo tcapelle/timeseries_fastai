6999ce7850e43f7a299a36ae32e44beca0c32032
Thomas Capelle <thomascapelle@gmail.com>
2019-10-03 16:23:45 +0200

    updates

--- res2net.py
+++ res2net.py
@@ -37,10 +37,7 @@ class Mish(nn.Module):
         super().__init__()
 
     def forward(self, x):  
-        #save 1 second per epoch with no x= x*() and then return x...just inline it.
         return x *( torch.tanh(F.softplus(x))) 
-        #return x * tanh(softplus(x)) = 
-        #return x * tanh(ln(1 + e^{x}))
         
 act_fn = Mish()
 
